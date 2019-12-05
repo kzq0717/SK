b97278d15507535634b74135b6524bb0c6684c89
SK <SK@DESKTOP-087TEQ0>
2019-12-05 18:39:57 +0800

    增加DataProcessHelper类：主要处理用于处理数据

---
+++ DataProcessHelper.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.IO.Compression;
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.InteropServices;
+
+
+
+namespace SK
+{
+    /// <summary>数据处理类
+    /// </summary>
+    public class DataProcessHelper
+    {
+
+        /// <summary>  字节数组压缩 
+        /// </summary>  
+        /// <param name="data"></param>  
+        /// <returns></returns>  
+        public static byte[] Compress(byte[] data)
+        {
+            try
+            {
+                if (data == null)
+                {
+                    throw new Exception("Data must be not null");
+                }
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true);
+                    zip.Write(data, 0, data.Length);
+                    zip.Close();
+                    byte[] buffer = new byte[ms.Length];
+                    ms.Position = 0;
+                    ms.Read(buffer, 0, buffer.Length);
+                    ms.Close();
+                    return buffer;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        /// <summary> 字节数组解压缩   
+        /// </summary>  
+        /// <param name="data"></param>  
+        /// <returns></returns>  
+        public static byte[] Decompress(byte[] data)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream(data);
+                GZipStream zip = new GZipStream(ms, CompressionMode.Decompress, true);
+                MemoryStream msreader = new MemoryStream();
+                byte[] buffer = new byte[0x1000];
+                while (true)
+                {
+                    int reader = zip.Read(buffer, 0, buffer.Length);
+                    if (reader <= 0)
+                    {
+                        break;
+                    }
+                    msreader.Write(buffer, 0, reader);
+                }
+                zip.Close();
+                ms.Close();
+                msreader.Position = 0;
+                buffer = msreader.ToArray();
+                msreader.Close();
+                return buffer;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        /// <summary> 字符串压缩
+        /// </summary>
+        /// <returns>The string.</returns>
+        /// <param name="str">String.</param>
+        public static string CompressString(string str)
+        {
+            string compressString = "";
+            byte[] compressBeforeByte = Encoding.UTF8.GetBytes(str);
+            byte[] compressAfterByte = Compress(compressBeforeByte);
+            //compressString = Encoding.GetEncoding("UTF-8").GetString(compressAfterByte);    
+            compressString = Convert.ToBase64String(compressAfterByte);
+            return compressString;
+        }
+
+        /// <summary> 字符串解压缩
+        /// </summary>
+        /// <returns>The string.</returns>
+        /// <param name="str">String.</param>
+        public static string DecompressString(string str)
+        {
+            string compressString = "";
+            //byte[] compressBeforeByte = Encoding.GetEncoding("UTF-8").GetBytes(str);    
+            byte[] compressBeforeByte = Convert.FromBase64String(str);
+            byte[] compressAfterByte = Decompress(compressBeforeByte);
+            compressString = Encoding.UTF8.GetString(compressAfterByte);
+            return compressString;
+        }
+
+        /// <summary>  将对象转换为byte数组
+        /// </summary>
+        /// <param name="obj">被转换对象</param>
+        /// <returns>转换后byte数组</returns>
+        public static byte[] Object2Bytes(object obj)
+        {
+            byte[] buff = new byte[Marshal.SizeOf(obj)];
+            IntPtr ptr = Marshal.UnsafeAddrOfPinnedArrayElement(buff, 0);
+            Marshal.StructureToPtr(obj, ptr, true);
+            return buff;
+        }
+
+        /// <summary>   将byte数组转换成对象
+        /// </summary>
+        /// <param name="buff">被转换byte数组</param>
+        /// <param name="typ">转换成的类名</param>
+        /// <returns>转换完成后的对象</returns>
+        public static object Bytes2Object(byte[] buff, Type typ)
+        {
+            IntPtr ptr = Marshal.UnsafeAddrOfPinnedArrayElement(buff, 0);
+            return Marshal.PtrToStructure(ptr, typ);
+        }
+
+
+        /// <summary>  将文件转换为byte数组
+        /// </summary>
+        /// <param name="path">文件地址</param>
+        /// <returns>转换后的byte数组</returns>
+        public static byte[] File2Bytes(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return new byte[0];
+            }
+
+            FileInfo fi = new FileInfo(path);
+            byte[] buff = new byte[fi.Length];
+
+            FileStream fs = fi.OpenRead();
+            fs.Read(buff, 0, Convert.ToInt32(fs.Length));
+            fs.Close();
+            return buff;
+        }
+
+        /// <summary> 将byte数组转换为文件并保存到指定地址
+        /// </summary>
+        /// <param name="buff">byte数组</param>
+        /// <param name="savepath">保存地址</param>
+        public static void Bytes2File(byte[] buff, string savepath)
+        {
+            if (File.Exists(savepath))
+            {
+                File.Delete(savepath);
+            }
+
+            FileStream fs = new FileStream(savepath, FileMode.CreateNew);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(buff, 0, buff.Length);
+            bw.Close();
+            fs.Close();
+        }
+
+    }
+}
